0e192a2d0fdf31a0ffb410d57e779c446785f3bd
Takuto Wada <takuto.wada@gmail.com>
2015-05-27 10:27:07 +0900

    refactor(unassertify): extract unassert module out

--- index.js
+++ index.js
@@ -13,58 +13,15 @@
 var through = require('through');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
-var estraverse = require('estraverse');
-var escallmatch = require('escallmatch');
-var patterns = [
-    'assert(value, [message])',
-    'assert.ok(value, [message])',
-    'assert.equal(actual, expected, [message])',
-    'assert.notEqual(actual, expected, [message])',
-    'assert.strictEqual(actual, expected, [message])',
-    'assert.notStrictEqual(actual, expected, [message])',
-    'assert.deepEqual(actual, expected, [message])',
-    'assert.notDeepEqual(actual, expected, [message])',
-    'assert.deepStrictEqual(actual, expected, [message])',
-    'assert.notDeepStrictEqual(actual, expected, [message])',
-    'assert.fail(actual, expected, message, operator)',
-    'assert.throws(block, [error], [message])',
-    'assert.doesNotThrow(block, [message])',
-    'assert.ifError(value)',
-    'console.assert(value, [message])'
-];
-var matchers = patterns.map(escallmatch);
+var unassert = require('unassert');
 
 function isDebugMode (filepath, options) {
     return (options && options._flags && options._flags.debug);
 }
 
-function matches (node) {
-    return function (matcher) {
-        return matcher.test(node);
-    };
-}
-
-function parentPath (controller) {
-    return controller.path().slice(0, -1).join('/');
-}
-
-function unassert (code, options) {
-    var pathToRemove = {};
+function applyUnassert (code, options) {
     var ast = esprima.parse(code);
-    estraverse.replace(ast, {
-        enter: function (currentNode, parentNode) {
-            if (matchers.some(matches(currentNode))) {
-                pathToRemove[parentPath(this)] = true;
-                this.skip();
-            }
-        },
-        leave: function (currentNode, parentNode) {
-            if (this.path() && pathToRemove[this.path().join('/')]) {
-                this.remove();
-            }
-        }
-    });
-    return escodegen.generate(ast);
+    return escodegen.generate(unassert(ast));
 }
 
 module.exports = function unassertify (filepath, options) {
@@ -80,7 +37,7 @@ module.exports = function unassertify (filepath, options) {
     }
 
     function end() {
-        stream.queue(unassert(data, options));
+        stream.queue(applyUnassert(data, options));
         stream.queue(null);
     }
 
